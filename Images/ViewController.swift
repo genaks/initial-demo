5f49df28349c95b0ef6596b4aadf847fa198365b
Akshay <Aksay@Aksays-MacBook-Pro.local>
2015-01-04 20:20:29 +0530

    Initial Commit

---
+++ Images/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Images
+//
+//  Created by Akshay on 04/01/15.
+//  Copyright (c) 2015 SweetPi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
